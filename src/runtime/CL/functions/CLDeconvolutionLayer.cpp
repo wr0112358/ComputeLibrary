55c3b22352f9b17711a5643a9df64e20795afbdb
Reiter Wolfgang <wr0112358@gmail.com>
2018-05-12 18:14:52 +0200

    bugfix: CLDeconvolutionLayer::validate fails if bias==NULL

--- src/runtime/CL/functions/CLDeconvolutionLayer.cpp
+++ src/runtime/CL/functions/CLDeconvolutionLayer.cpp
@@ -62,8 +62,8 @@ Status CLDeconvolutionLayer::validate(const ITensorInfo *input, const ITensorInf
 
     const TensorShape output_shape = deconvolution_output_shape(out_dims, input->tensor_shape(), weights->tensor_shape());
 
-    ARM_COMPUTE_RETURN_ERROR_ON_MISMATCHING_DATA_TYPES(input, output, weights, bias);
-    ARM_COMPUTE_RETURN_ERROR_ON_MISMATCHING_FIXED_POINT(input, output, weights, bias);
+    ARM_COMPUTE_RETURN_ERROR_ON_MISMATCHING_DATA_TYPES(input, output, weights);
+    ARM_COMPUTE_RETURN_ERROR_ON_MISMATCHING_FIXED_POINT(input, output, weights);
 
     if(bias != nullptr)
     {
@@ -78,7 +78,6 @@ Status CLDeconvolutionLayer::validate(const ITensorInfo *input, const ITensorInf
     TensorInfo scale_out_info(input->clone()->set_is_resizable(true).reset_padding().set_tensor_shape(compute_deconvolution_shape(*input, stride_x, stride_y, inner_border_right, inner_border_top,
                                                                                                       info)));
     const PadStrideInfo conv_info(1, 1, 0, 0, 0, 0, DimensionRoundingType::CEIL);
-
     ARM_COMPUTE_RETURN_ON_ERROR(CLDeconvolutionLayerUpsample::validate(input, &scale_out_info, BorderSize(inner_border_right, inner_border_top), info));
     ARM_COMPUTE_RETURN_ON_ERROR(CLConvolutionLayer::validate(&scale_out_info, weights, bias, output, info, WeightsInfo()));
 
